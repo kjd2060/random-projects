a826d9594dfbc9c9acbf7fb6caa851657c9f284e
kjd2060 <gamelord317@gmail.com>
2016-08-16 15:16:56 -0400

    Update e_displayer.py

--- math/e_displayer.py
+++ math/e_displayer.py
@@ -5,8 +5,8 @@ while True:
 	digitsStr = input("How many digits to display? ")
 	try:
 		digits = int(digitsStr)
-		if((digits > 0) and (digits < gMaxDigitsAllowed)):
-			lengthString = "{0:." + str(digits) + "f}"
+		if((digits > 0) and (digits < gMaxDigitsAllowed)): # we're only accurate to a trail of so many digits, so stop after that
+			lengthString = "{0:." + str(digits) + "f}" # create the initial string object that we can attach the num to
 			print(lengthString.format(math.e))
 			break
 		elif(digits > gMaxDigitsAllowed - 1):
