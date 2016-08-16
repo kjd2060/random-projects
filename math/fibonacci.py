1830f0aea732c438886ca4d88c161833694a9b32
kjd2060 <gamelord317@gmail.com>
2016-08-16 13:47:34 -0400

    Add files via upload

--- math/fibonacci.py
+++ math/fibonacci.py
@@ -1,26 +1,34 @@
-import math
+import math, textwrap
 
 while True:
 	max_num_str = input("How far into the sequence should I go? ")
 	try:
 		max_num = int(max_num_str)
-		n = 1
-		index = 0
-		previous = 0
-		ar = []
-		ar.append(n)
-		while True:
-			if(index < max_num or max_num == n):
-				temp = n
-				n = n + previous # next = current + previous, or Fn+1 = Fn + Fn-1 or Fn = Fn-1 + Fn-2
-				previous = temp
-				index += 1
-				ar.append(n)
-			else:
-				for num in ar:
-					print("{:d}, ".format(num), end="")
-				print("Done!")
-				break
-		break
+		if(max_num <= 150 and max_num >= 0):
+			n = 1
+			index = 0
+			previous = 0
+			ar = []
+			ar.append(n)
+			while True:
+				if(index < max_num and max_num != n):
+					temp = n
+					n = n + previous # next = current + previous, or Fn+1 = Fn + Fn-1 or Fn = Fn-1 + Fn-2
+					previous = temp
+					index += 1
+					ar.append(n)
+				else:
+					num_str = ""
+					for num in ar:
+						num_str += str(num)
+						num_str += ", "
+					print(textwrap.fill(num_str, 75), end="")
+					print(" Done!")
+					break
+			break
+		else:
+			print("Please input a number between 0-150")
+
 	except ValueError:
 		print("Please only enter a positive whole number")
+	
