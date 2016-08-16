faf28406a4c45b39c7c9fa7dd90c1a213fb9d730
kjd2060 <gamelord317@gmail.com>
2016-08-16 14:04:36 -0400

    Update pi_displayer.py

--- math/pi_displayer.py
+++ math/pi_displayer.py
@@ -1,16 +1,19 @@
 import math
 
+gMaxDigitsAllowed = 49
+
 while True:
 	digitsStr = input("How many digits to display? ")
 	try:
 		digits = int(digitsStr)
-		if((digits > 0) and (digits < 49)):
+		if((digits > 0) and (digits < gMaxDigitsAllowed)):
 			lengthString = "{0:." + str(digits) + "f}"
 			print(lengthString.format(math.pi))
 			break
-		elif(digits > 48):
+		elif(digits > gMaxDigitsAllowed - 1):
 			print("Not accurate out to that many digits; here's the most we can guarantee")
-			print("{0:.48f}".format(math.pi))
+			temp = "{0:." + str(gMaxDigitsAllowed-1) + "f}"
+			print(temp.format(math.pi))
 		else:
 			print("Please enter a non negative number")
 	except ValueError:
