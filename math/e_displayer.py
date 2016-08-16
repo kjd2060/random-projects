bac7499fed2492afcdf4b4d370e65a6aca85086d
kjd2060 <gamelord317@gmail.com>
2016-08-16 14:01:45 -0400

    Update e_displayer.py

--- math/e_displayer.py
+++ math/e_displayer.py
@@ -1,16 +1,18 @@
 import math
 
+gMaxDigitsAllowed = 49
 while True:
 	digitsStr = input("How many digits to display? ")
 	try:
 		digits = int(digitsStr)
-		if((digits > 0) and (digits < 49)):
+		if((digits > 0) and (digits < gMaxDigitsAllowed)):
 			lengthString = "{0:." + str(digits) + "f}"
 			print(lengthString.format(math.e))
 			break
-		elif(digits > 48):
+		elif(digits > gMaxDigitsAllowed - 1):
 			print("Not accurate out to that many digits; here's the most we can guarantee")
-			print("{0:.48f}".format(math.e))
+			temp = "{0:." + str(gMaxDigitsAllowed - 1) + "f}"
+			print(temp.format(math.e))
 		else:
 			print("Please enter a non negative number")
 	except ValueError:
