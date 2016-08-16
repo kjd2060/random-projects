8b8adb448950a602546fbd002a55782ddbf8ebcd
kjd2060 <gamelord317@gmail.com>
2016-08-16 14:02:28 -0400

    Update factorial.py

--- math/factorial.py
+++ math/factorial.py
@@ -1,5 +1,7 @@
 import math
 
+gMaxAllowed = 100
+
 def factorial(n):
 	if(n == 1):
 		return 1
@@ -10,11 +12,11 @@ while True:
 	input_str = input("What should I compute the factorial of? ")
 	try:
 		n = int(input_str)
-		if(n <= 100 and n > 0):
+		if(n <= gMaxAllowed and n > 0):
 			fact = factorial(n)
 			print(fact)
 			break
 		else:
-			print("Please enter a positive number no greater than 100....gets large fast otherwise")
+			print("Please enter a positive number no greater than" + str(gMaxAllowed) + "...gets large fast otherwise")
 	except ValueError:
 		print("Please only enter a positive number")
