22f280859f4360eb8960563dc0532e4ce120bb60
kjd2060 <gamelord317@gmail.com>
2019-02-13 16:50:05 -0500

    Add files via upload

---
+++ Java/java_program.java
@@ -0,0 +1,26 @@
+/*
+ * quick pass at this.  this will work but I can't run it cause don't have jdk on this computer
+*/
+
+// this class should be its own file...eg DataClass.java
+public class DataClass{ // was there a class name?
+    double rate10 = 0; // correct variable names?
+    double rate5 = 0;
+
+    // not setting variables with instantiation?
+    public DataClass(){
+        rate10 = 10.00;
+        rate5 = 5.00;
+        displayRates(); // print the things
+    }
+
+    private void displayRates(){
+        // should this loop?
+        System.out.println("rate10: " + rate10 + ", rate5: " + rate5);
+    }
+
+    // you should seperate this out into its own file; e.g. Main.java
+    public static void main(String[] args){
+        DataClass temp = new DataClass(); // should start printing immediately
+    }
+}
